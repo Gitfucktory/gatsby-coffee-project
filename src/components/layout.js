cedc81fe936e89a08fdc5d22c656f90a911187d4
Aron <arondomains@gmail.com>
2021-03-01 17:34:35 +0200

    initial commit

---
+++ src/components/layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './bootstrap.min.css';
+import "./layout.css";
+
+
+const Layout = ({ children }) => (
+    <>{children}</>
+);
+
+export default Layout;
+
+
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { FaGripfire } from "react-icons/fa";
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={['gatsby',
+    'application', 'react']} />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <FaGripfire />
+  </Layout>
+);
+
+export default IndexPage;
